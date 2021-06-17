d28db94fcc8666274e7c6b3fb78bd3a20ca92cc7
Eli Ronaldo <elironaldogonzalez@gmail.com>
2021-06-16 19:00:36 -0700

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,20 +8,36 @@ import { InfoPagina } from '../pages/interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
-  cargada: boolean = false;
+  cargada = false;
+
+  equipo: any[] = [];
+  resp: any[] = [];
 
   constructor( private http: HttpClient) { 
     console.log("servicio de infoPagina cargado");
-
-      //Leer archivo JSON
-      this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) =>{
-
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-
-      });
+    this.cargarInfo();
+    this.cargarEquipo();
   }
 
+  private cargarInfo(){
+          //Leer archivo JSON
+          this.http.get('assets/data/data-pagina.json')
+          .subscribe( (resp: InfoPagina) =>{
+    
+            this.cargada = true;
+            this.info = resp;
+            console.log(resp);
+    
+          });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-cebdf-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+ 
+      this.equipo = resp;
+      console.log(resp);
+
+    });
+  }
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Producto } from '../pages/interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+
+  productos: Producto[] = [];
+
+  constructor( private http: HttpClient) { 
+    this.cargarProductos();
+  }
+
+  private cargarProductos(){
+
+    this.http.get('https://angular-html-cebdf-default-rtdb.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: any) => {
+
+        console.log(resp)
+        this.productos = resp;
+        this.cargando = false;
+      });
+  }
+}
