3326e3b95f0d4a586925791e6ab09f47aaa98a97
Eli Ronaldo <elironaldogonzalez@gmail.com>
2021-06-16 20:18:02 -0700

    fin de seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,8 +8,8 @@ import { Producto } from '../pages/interfaces/producto.interface';
 export class ProductosService {
 
   cargando = true;
-
   productos: Producto[] = [];
+  productosFiltrados: Producto[] = [];
 
   constructor( private http: HttpClient) { 
     this.cargarProductos();
@@ -17,12 +17,46 @@ export class ProductosService {
 
   private cargarProductos(){
 
-    this.http.get('https://angular-html-cebdf-default-rtdb.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: any) => {
+    return new Promise<void>( (resolve, reject) =>{
 
-        console.log(resp)
+      this.http.get('https://angular-html-cebdf-default-rtdb.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: any) => {
         this.productos = resp;
         this.cargando = false;
+        resolve();
+      }); 
+    });
+
+  }
+  
+   getProducto(id: string){
+    return this.http.get(`https://angular-html-cebdf-default-rtdb.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ){
+    
+    if (this.productos.length === 0){
+
+      this.cargarProductos().then( () => {
+        this.filtrarProductos(termino);
       });
+    }
+    else
+    {
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos( termino: string ) {
+    this.productosFiltrados = [];
+
+    termino = termino.toLowerCase();
+
+     this.productos.forEach(prod => {
+       const tituloLower = prod.titulo.toLocaleLowerCase();
+       if( prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0){
+          this.productosFiltrados.push( prod );
+       }
+     })
   }
 }
